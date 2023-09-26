ec4137d584cef4ad60c250e0a0b5bcc77038e6f3
Ron Bruce <ronbruce@Rons-MacBook-Pro.local>
2023-09-26 16:59:52 -0400

    I added a butto my mood cards

--- scripts.js
+++ scripts.js
@@ -21,6 +21,12 @@ for (let i = 0; i < data.length; i += 1) {
     const price = document.createElement('p')
     price.innerText = data[i].price
     newDiv.appendChild(price)
+
+    const button = document.createElement('button')
+    button.id = data[i].name
+    button.dataset.price = data[i].price
+    button.innerHTML = 'Add to Cart'
+    newDiv.appendChild(button)
     // Now we will render everything we created above to the browser! 
     itemsContainer.appendChild(newDiv)
 }
